7d68cef1f5ba3f3b45ebdf2424cdc4e65de4dece
Bailey Mejia <baileymejia28@gmail.com>
2022-10-04 18:52:17 -0700

    Created a new repository with javascript, css, and html. Added pseudocoding for the javascript and added code for styling

---
+++ script.js
@@ -0,0 +1,13 @@
+//  need to add code for start button to start game
+//add code for timer to reset on start
+
+// Add a function for the gameplay like rock paper scissors
+//by filling in blanks that match the number of letters in that word
+//have to use key events
+
+//I need to add a timer so the game is tied
+
+//Have to keep track of user score
+
+
+
