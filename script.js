7dd819fe8f037493cd180c9d8476356d1f9357cf
Bailey Mejia <baileymejia28@gmail.com>
2022-10-14 18:20:13 -0700

    Added an array and made it random every time you click start

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-//  need to add code for start button to start game
-//add code for timer to reset on start
+// Guessing Word Game
+// Call variables to link the classes and ID
+var blankWords = document.querySelector('.word-blanks');
+var wins = document.querySelector('.win');
+var losses = document.querySelector('.losses');
+var timeEl = document.querySelector('.seconds-remaining');
+var start = document.querySelector('#Start');
+var reset = document.querySelector('.reset-btn');
 
-// Add a function for the gameplay like rock paper scissors
-//by filling in blanks that match the number of letters in that word
-//have to use key events
 
-//I need to add a timer so the game is tied
+//More variabkes for actual details of the game
+var userWord = '';
+var blanksInWord = 0;
+var winCounter = 0;
+var loseCounter = 0;
+isWin = false;
 
-//Have to keep track of user score
+//Make an array to create blanks and letters on screen
+var lettersInWords = [];
+var blanks = [];
 
+//Array of words
+var arrayOfWords = ['javascript', 'array', 'object', 'function', 'string', 'boolean', 'Modulas'];
+var mathArray = Math.floor(Math.random () * arrayOfWords.length);
+var selectArrayChoices = arrayOfWords[mathArray];
+console.log(selectArrayChoices);
+
+//Create a start game function when the button is clicked
+function startGame(){
+    isWin = false;
+}
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+
+
+// Create blanks on screen with a function, use math
+
+//Create event Listener for start button
 
 
